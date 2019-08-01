9c5a71b24ad728fbb602bd8ed1fa463ae52bebd2
MuTsunTsai <don.m.t.tsai@gmail.com>
2019-08-02 06:43:11 +0800

    Add comments to source code

--- test/vue-on-clickout.js
+++ test/vue-on-clickout.js
@@ -11,7 +11,10 @@
 const VueOnClickout = {
 	install(Vue) {
 		const cout = "clickout";
+		
+		// Keeps a list of all elements with v-clickout directive.
 		let clickoutList = [];
+
 		Vue.directive(cout, {
 			bind(el) {
 				clickoutList.push(el);
@@ -19,6 +22,8 @@ const VueOnClickout = {
 				let add = el.addEventListener.bind(el);
 				add('click', () => el.__clickoutFlag = true);
 				add(cout, (e) => {
+					// In case of stopPropagation, prevent any ancestor element
+					// from firing the clickout event.
 					if(e.cancelBubble) clickoutList.forEach(p => {
 						if(p.contains(el)) p.__clickoutFlag = true;
 					});
@@ -29,12 +34,14 @@ const VueOnClickout = {
 			}
 		});
 
+		// Sort the elements in the list so that the events will fire in the correct order.
 		function elSort(e1, e2) {
 			if(e2.contains(e1)) return -1;
 			if(e1.contains(e2)) return 1;
 			return 0;
 		}
 
+		// Use a central event listener to control the firing of clickout events.
 		document.addEventListener('click', () => {
 			clickoutList.sort(elSort);
 			for(let el of clickoutList) {
@@ -43,6 +50,13 @@ const VueOnClickout = {
 			}
 		});
 
+		// The trick is that we hack the element-creating method of Vue,
+		// and replace it with a wrapper function that detects the existence
+		// of the "v-on:clickout" event. If the event exists, then add the
+		// "v-clickout" directive to it.
+		// We still use directive as in many other similar packages,
+		// because that's the most convenient way of tracking the construction
+		// and destruction of elements.
 		Vue.mixin({
 			beforeCreate() {
 				let original_c = this._c;
@@ -60,6 +74,7 @@ const VueOnClickout = {
 	}
 }
 
+// Automatically load the plugin itself.
 if(typeof window !== 'undefined' && window.Vue) {
 	window.Vue.use(VueOnClickout);
 }
