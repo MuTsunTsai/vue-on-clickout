dbec169703f43e47cdd19ecbe88ed32f67c24e23
MuTsunTsai <don.m.t.tsai@gmail.com>
2019-08-02 06:08:00 +0800

    Fix the event firing order

--- test/vue-on-clickout.js
+++ test/vue-on-clickout.js
@@ -10,30 +10,38 @@
 
 const VueOnClickout = {
 	install(Vue) {
-		const event = "clickout";
-		Vue.directive(event, {
+		const cout = "clickout", stop = "clickoutstop";
+		let clickoutList = [];
+		Vue.directive(cout, {
 			bind(el) {
+				clickoutList.push(el);
 				el.__clickoutFlag = false;
-				el.__clickoutStop = false;
-				el.addEventListener('click', () => el.__clickoutFlag = true);
-				el.addEventListener(event, (e) => {
-					if(e.cancelBubble) {
-						let p = el;
-						do {
-							p = p.parentNode;
-							p.__clickoutStop = true;
-						} while(p != document.body);
-					}
-				})
-				el.__clickoutHandler = () => {
-					if(!el.__clickoutFlag && !el.__clickoutStop) el.dispatchEvent(new Event(event));
-					el.__clickoutFlag = false;
-					el.__clickoutStop = false;
-				};
-				document.addEventListener('click', el.__clickoutHandler);
+				let add = el.addEventListener.bind(el);
+				add('click', () => el.__clickoutFlag = true);
+				add(cout, (e) => {
+					if(e.cancelBubble) el.dispatchEvent(new Event(stop, { bubbles: true }));
+				});
+				add(stop, () => el.__clickoutFlag = true);
 			},
 			unbind(el) {
-				document.removeEventListener('click', el.__clickoutHandler);
+				clickoutList.splice(clickoutList.indexOf(el), 1);
+			}
+		});
+
+		function getDepth(el) {
+			let p = el, dep = 0;
+			while(p != document.body && p != null) {
+				p = p.parentNode; dep++;
+			}
+			el.__clickoutDepth = dep;
+		}
+
+		document.addEventListener('click', () => {
+			clickoutList.forEach(e => getDepth(e));
+			clickoutList.sort((e1, e2) => e2.__clickoutDepth - e1.__clickoutDepth);
+			for(let el of clickoutList) {
+				if(!el.__clickoutFlag) el.dispatchEvent(new Event(cout));
+				el.__clickoutFlag = false;
 			}
 		});
 
@@ -41,10 +49,10 @@ const VueOnClickout = {
 			beforeCreate() {
 				let original_c = this._c;
 				function hack_c(a, b, c, d) {
-					if(b && b.on && b.on[event]) {
+					if(b && b.on && b.on[cout]) {
 						b.directives = b.directives || [];
-						if(!b.directives.some(d => d.name == event))
-							b.directives.push({ name: event, rawName: "v-" + event });
+						if(!b.directives.some(d => d.name == cout))
+							b.directives.push({ name: cout, rawName: "v-" + cout });
 					}
 					return original_c(a, b, c, d);
 				}
